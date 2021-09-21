3a92b0d1aedee30227d9b13bb458983331e22beb
Luis Mario <luismario.valdiviesos@gmail.com>
2021-09-21 10:44:11 -0500

    hasta que carga modal en calendario para agendar

--- app/Http/Livewire/CalendarioController.php
+++ app/Http/Livewire/CalendarioController.php
@@ -3,14 +3,49 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+use App\Models\Paciente;
+use App\Models\Medico;
+use App\Models\User;
+use App\Models\Tratamiento;
+use App\Models\Pago;
+use App\Models\Estado;
+use Carbon\Carbon;
+use App\Models\Cita;
+use Illuminate\Support\Facades\Auth;
 
 class CalendarioController extends Component
 {
-    public $buscar_paciente, $fecha_ini, $fecha_fin;
+    public $descripcion, $fecha_ini, $fecha_fin, $medico_id, $receta, $user_id, $buscar_paciente,
+    $tratamiento_id, $pago_id, $citas ;
+
+
+    public function mount()
+    {
+        $this->estado_id = 0;
+        $this->citas = [];
+
+
+    }
+    public function save()
+    {
+        // dd($this->fecha_ini, $this->fecha_fin, $this->buscar_paciente, $this->descripcion, $this->medico_id, $this->receta,
+        //  $this->user_id, $this->tratamiento_id, $this->pago_id);
+    }
 
     public function render()
     {
-        return view('livewire.calendario.component')
+        return view('livewire.calendario.component', [
+            'estados' => Estado::orderBy('id','asc')->get(),
+            'citas' => $this->citas,
+            'pacientesSearch' => Paciente::all(),
+            'pacientes' => Paciente::where('nombre','like','%'.$this->buscar_paciente.'%')->get(),
+            'medicos' => Medico::all(),
+            'tratamientos' => Tratamiento::all(),
+            'pagos' => Pago::all(),
+            'estados' => Estado::all()
+        ])
         ->extends('layouts.theme.app')
         ->section('content');
     }
--- resources/views/livewire/calendario/component.blade.php
+++ resources/views/livewire/calendario/component.blade.php
@@ -1,54 +1,182 @@
-<div class="container" id="calendar" wire:ignore>
-</div>
+<div wire:ignore>
 
-{{-- MODAL --}}
-
-
-
-<div class="modal fade" id="theModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form>
-            <div class="form-group">
-              <label for="recipient-name" class="col-form-label">Recipient:</label>
-              <input type="text" class="form-control" id="recipient-name">
-            </div>
-            <div class="form-group">
-              <label for="message-text" class="col-form-label">Message:</label>
-              <textarea class="form-control" id="message-text"></textarea>
-            </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Send message</button>
-        </div>
-      </div>
+    <div class="container" id="calendar">
     </div>
-  </div>
+
+    {{-- MODAL --}}
 
 
-{{-- MODAL --}}
 
+    <div class="modal fade" id="theModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Angendar Cita</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+            <form wire:submit.prevent='save'>
+                <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label >Paciente</label>
+                            <input wire:model="buscar_paciente" type="search" class="form-control" placeholder="Buscar paciente en el sistema ">
+                            @error('buscar_paciente') <span class="text-danger er">{{ $message }}</span> @enderror
+                            {{-- @if ($buscar_paciente != '')
+                            <ul class="list-group" >
+                                @if (!$pacientes->isEmpty())
+                                    @foreach($pacientes as $p)
+                                        <li wire:click="cargarPaciente('{{$p}}')"
+                                            class="list-group lis-group-action"
+                                         >
+                                            {{ $p->nombre }}</li>
+                                    @endforeach
+                                @else
+                                    @if ($editar == 'si')
+                                        <p>Ups! No hay resultados </p>
+                                    @else
+                                        <p>Ups! No hay resultados </p>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                            Crear Paciente
+                                        </button>
+                                    @endif
+                                @endif
+
+                            </ul>
+                        @else
+                            <p>No hay consulta </p>
+                        @endif --}}
+                        </div>
+                    </div>
+                       <div class="mt-2 col-sm-6">
+                            <h6>Fecha Inicio</h6>
+                            <div class="form-group">
+                                <input type="text" wire:model='fecha_ini'
+                                class="form-control flatpickr"
+                                placeholder="Click para elegir">
+                            </div>
+                        </div>
+
+                        <div class="mt-2 col-sm-6">
+                            <h6>Fecha Final</h6>
+                            <div class="form-group">
+                                <input type="text" wire:model='fecha_fin'
+                                class="form-control flatpickr"
+                                placeholder="Click para elegir">
+                            </div>
+                        </div>
+
+                        <div class="col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <label >Descripción</label>
+                                <input type="text" wire:model.lazy="descripcion" class="form-control" >
+                                @error('descripcion') <span class="text-danger er">{{ $message }}</span> @enderror
+                            </div>
+                        </div>
+
+
+
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <label >Medico</label>
+                                <select wire:model.lazy="medico_id" class="form-control">
+                                    {{-- <option value="Elegir" selected>Elegir</option>
+                                    @foreach ($medicos as $m )
+                                    <option value="{{ $m->id }}" >{{ $m->nombre }}</option>
+                                    @endforeach --}}
+                                </select>
+                                @error('medico_id') <span class="text-danger er">{{ $message }}</span> @enderror
+                            </div>
+                        </div>
+
+                            <div class="col-sm-12 col-md-6">
+                                <div class="form-group">
+                                    <label >Receta</label>
+                                    <textarea wire:model.lazy='receta' class="form-control"  rows="3"></textarea>
+
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label >Tratamiento</label>
+                                    <select wire:model.lazy="tratamiento_id" class="form-control">
+                                        <option value="Elegir" selected>Elegir</option>
+                                        {{-- @foreach ($tratamientos as $t )
+                                        <option value="{{ $t->id }}" >{{ $t->nombre }}</option>
+                                        @endforeach --}}
+                                    </select>
+                                    @error('tratamiento_id') <span class="text-danger er">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 col-md-6">
+                                <div class="form-group">
+                                    <label >Pagos</label>
+                                    <select wire:model.lazy="pago_id" class="form-control">
+                                        <option value="Elegir" selected>Elegir</option>
+                                        {{-- @foreach ($pagos as $p)
+                                        <option value="{{ $p->id }}" >{{ $p->nombre }}</option>
+                                        @endforeach --}}
+                                    </select>
+                                    @error('pago_id') <span class="text-danger er">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 col-md-6">
+                                <div class="form-group">
+                                    <label >Estado Cita</label>
+                                    <select wire:model.lazy="estado" class="form-control">
+                                        <option value="Elegir" selected>Elegir</option>
+                                        {{-- @foreach ($estados as $e)
+                                        <option value="{{ $e->id }}" >{{ $e->nombre }}</option>
+                                        @endforeach --}}
+                                    </select>
+                                    @error('estado') <span class="text-danger er">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+
+                </div>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button class="btn btn-primary">Registrar</button>
+
+            </form>
+            </div>
+
+        </div>
+        </div>
+    </div>
+
+
+    {{-- MODAL --}}
+
+</div>
 @push('script')
 
 <script>
-
+    // $('#theModal').on("hidden.bs.modal", function(){
+    //     @this.title = '';
+    //     @this.start = '';
+    //     @this.end = '';
+    // })
     document.addEventListener('DOMContentLoaded', function(){
         const calendarEL =  document.getElementById('calendar');
         const calendar = new FullCalendar.Calendar(calendarEL, {
             initialView: 'dayGridMonth',
             locale: 'es',
             selectable:true,
-            select: function({startStr, endStr}){
-                console.log(startStr);
+            select: function({startStr, endStr, buscar_paciente, descripcion, medico_id, receta, user_id, tratamiento_id, pago_id }){
+                @this.fecha_ini = startStr;
+                @this.fecha_fin = endStr;
+                @this.buscar_paciente = buscar_paciente;
+                @this.descripcion = descripcion;
+                @this.medico_id = medico_id;
+                @this.receta =  receta;
+                @this.user_id = user_id;
+                @this.tratamiento_id = tratamiento_id;
+                @this.pago_id =  pago_id;
                $('#theModal').modal('toggle');
             }
         });
