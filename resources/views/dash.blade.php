bb318122173c8b9d566422cc10d609a5e4330989
luismariovaldiviesos <luismario.valdiviesos@gmail.com>
2023-09-28 09:35:22 -0500

    cambios al dash

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -138,6 +138,60 @@
 
     </div>
 
+    <div class="row">
+
+        <div class="col-12">
+
+            <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="d-flex flex-column flex-md-row align-items-center justify-content-between">
+                      <div class="statistics-item">
+                        <p class="text-dark">
+                          <i class="icon-sm fa fa-user mr-2"></i>
+                         CITAS ATENDIDAS
+                        </p>
+                        <h2>{{ $finalizadas }}</h2>
+
+                      </div>
+                      <div class="statistics-item">
+                          <p class="text-dark">
+                            <i class="icon-sm fas fa-cloud-download-alt mr-2"></i>
+                           EN PROCESO
+                          </p>
+                          <h2>{{ $enproceso }}</h2>
+
+                        </div>
+                      <div class="statistics-item">
+                        <p class="text-dark">
+                          <i class="icon-sm fas fa-hourglass-half mr-2"></i>
+                         CITAS PENDIENTES
+                        </p>
+                        <h2>{{ $pendientes }}</h2>
+                      </div>
+                      <div class="statistics-item">
+                        <p class="text-dark">
+                          <i class="icon-sm fas fa-chart-line mr-2"></i>
+                          TOTAL CITAS AÑO {{ date('Y') }}
+                        </p>
+                        <h2>{{ $totalcitas }}</h2>
+
+                      </div>
+                      <div class="statistics-item">
+                        <p>
+                          <i class="icon-sm fas fa-circle-notch mr-2"></i>
+                          SALDOS PENDIENTES
+                        </p>
+                        <h2>${{ $totalSaldos }}</h2>
+
+                      </div>
+                  </div>
+                </div>
+              </div>
+
+        </div>
+
+    </div>
+
 </div>
 
 
